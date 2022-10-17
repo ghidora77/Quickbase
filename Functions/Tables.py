c51ed86411d71aa033872a0ef19e1027eefc0b5a
ghidora77 <ozzycampos@yahoo.com>
2022-10-17 19:30:51 -0400

    Update with new code

--- Functions/Tables.py
+++ Functions/Tables.py
@@ -1,21 +1,19 @@
 import json
 import requests
 
+
 def CreateTables(tableName, description, singleRecordName, pluralRecordName, domain, key, appID):
     """
     `CreateTables` function will create the tables within the specified Quickbase app.
-
-    This is typically run only one time, when starting the app, 
-    although it can be used for testing. 
+    This is typically run only one time, when starting the app,
+    although it can be used for testing.
     """
-
     body = {
         "name": tableName,
         "description": description,
         "singleRecordName": singleRecordName,
         "pluralRecordName": pluralRecordName
     }
-
     headers = {
         'QB-Realm-Hostname': domain,
         'User-Agent': '{User-Agent}',
@@ -24,20 +22,19 @@ def CreateTables(tableName, description, singleRecordName, pluralRecordName, dom
     params = {
         'appId': appID
     }
-
     body = body
     r = requests.post(
-        'https://api.quickbase.com/v1/tables', 
-        params = params, 
-        headers = headers, 
-        json = body
+        'https://api.quickbase.com/v1/tables',
+        params=params,
+        headers=headers,
+        json=body
     )
-
     return print(json.dumps(r.json(),indent=4))
+
 
 def DeleteTables(domain, appID, tableID, key):
     """
-    `DeleteTables` function will delete the specified table. 
+    `DeleteTables` function will delete the specified table.
     """
 
     headers = {
@@ -48,10 +45,10 @@ def DeleteTables(domain, appID, tableID, key):
     params = {
         'appId': appID
     }
-    r = requests.delete(
-    ('https://api.quickbase.com/v1/tables/' + tableID), 
-    params = params, 
-    headers = headers
+    r = requests.delete((
+            'https://api.quickbase.com/v1/tables/' + tableID
+    ),
+        params=params,
+        headers=headers
     )
-
     return print((json.dumps(r.json(),indent=4)))
